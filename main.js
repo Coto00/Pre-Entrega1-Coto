b37d7fef33a45c18e418d280553b89dad6311f69
Coto <thomascoto23@gmail.com>
2023-11-01 20:59:57 -0300

    commit2

--- main.js
+++ main.js
@@ -24,28 +24,28 @@ function Usuario() {
             } else if (opcion === null) {
                 alert('Opcion Inexistente');
             } else if (opcion === "1") {
-                let colorRemera = prompt('De qué color quieres tu Remera');
+                let colorRemera = prompt('De qué color quieres tu Remera').toLowerCase();
                 if (colorRemera === "rojo" || colorRemera === "verde" || colorRemera === "azul" || colorRemera === "negro") {
                     alert('Te enviaremos información de tu remera de color ' + colorRemera + ' a tu correo.');
                 } else {
                     alert('Color desconocido');
                 }
             } else if (opcion === "2") {
-                let colorBuzo = prompt('De qué color quieres tu Buzo');
+                let colorBuzo = prompt('De qué color quieres tu Buzo').toLowerCase();
                 if (colorBuzo === "rojo" || colorBuzo === "verde" || colorBuzo === "azul" || colorBuzo === "negro") {
                     alert('Te enviaremos información de tu buzo de color ' + colorBuzo + ' a tu correo.');
                 } else {
                     alert('Color desconocido');
                 }
             } else if (opcion === "3") {
-                let colorCampera = prompt('De qué color quieres tu Campera');
+                let colorCampera = prompt('De qué color quieres tu Campera').toLowerCase();
                 if (colorCampera === "rojo" || colorCampera === "verde" || colorCampera === "azul" || colorCampera === "negro") {
                     alert('Te enviaremos información de tu campera de color ' + colorCampera + ' a tu correo.');
                 } else {
                     alert('Color desconocido');
                 }
             } else if (opcion === "4") {
-                let colorPantalon = prompt('De qué color quieres tu Pantalón');
+                let colorPantalon = prompt('De qué color quieres tu Pantalón').toLowerCase();
                 if (colorPantalon === "rojo" || colorPantalon === "verde" || colorPantalon === "azul" || colorPantalon === "negro") {
                     alert('Te enviaremos información de tu pantalón de color ' + colorPantalon + ' a tu correo.');
                 } else {
